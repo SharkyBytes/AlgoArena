9718d2cfe36514fa4d16ab03b9fe0cb53d0acb69
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-05-21 12:31:34 +0530

    Time: 60 ms (30.82%) | Memory: 12.8 MB (57.38%) - LeetSync

--- 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.cpp
+++ 2061-painting-a-grid-with-three-different-colors/painting-a-grid-with-three-different-colors.cpp
@@ -1,57 +1,53 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
+int mod=1e9+7;
 
-    void generate(int m, vector<vector<int>>& patterns, vector<int>& curr) {
-        if (curr.size() == m) {
-            patterns.push_back(curr);
-            return;
-        }
-
-        for (int color = 0; color < 3; ++color) {
-            if (curr.empty() || curr.back() != color) {
-                curr.push_back(color);
-                generate(m, patterns, curr);
-                curr.pop_back();
-            }
-        }
+int cnt(int m , int n, int r, int c, int curr_state, int prev_state){
+    if(c==n){
+        return 1;
     }
 
+    if(r==m){
+        return cnt(m, n, 0, c+1, 0, curr_state);
+    }
+    if(r==0 && dp[c][prev_state]!=-1){
+        return dp[c][prev_state];
+    }
+
+    int up_row=0;
+    if(r>0){
+        up_row= curr_state&3;
+    }
+    int left=0;
+    if(c>0){
+        int mask= (m-r-1)*2;
+        left= (prev_state>>mask)&3;
+    }
+
+    int ways=0;
+
+    for(int i=1; i<=3;  i++){
+        if(i!=up_row && i!= left){
+         ways= (ways+ cnt(m, n, r+1, c, (curr_state<<2) + i , prev_state) )%mod;
+            
+        }
+    }
+   
+   if(r==0){
+    dp[c][prev_state]=ways;
+
+   }
+
+return ways;
+
+}
+
+    int dp[1000+2][1023];
     int colorTheGrid(int m, int n) {
-        vector<vector<int>> patterns;
-        vector<int> curr;
-        generate(m, patterns, curr);
+        memset(dp,-1, sizeof(dp));
 
-        int sz = patterns.size();
-        vector<vector<int>> valid(sz);
-
-        for (int i = 0; i < sz; ++i) {
-            for (int j = 0; j < sz; ++j) {
-                bool ok = true;
-                for (int k = 0; k < m; ++k) {
-                    if (patterns[i][k] == patterns[j][k]) {
-                        ok = false;
-                        break;
-                    }
-                }
-                if (ok) valid[i].push_back(j);
-            }
-        }
-
-        vector<int> dp(sz, 1);
-
-        for (int i = 1; i < n; ++i) {
-            vector<int> new_dp(sz, 0);
-            for (int j = 0; j < sz; ++j) {
-                for (int k : valid[j]) {
-                    new_dp[j] = (new_dp[j] + dp[k]) % mod;
-                }
-            }
-            dp = new_dp;
-        }
-
-        int result = 0;
-        for (int val : dp) result = (result + val) % mod;
-        return result;
+        return cnt(m,n,0,0,0,0);
+        
+        
     }
 };
