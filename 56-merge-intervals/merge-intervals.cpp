481fe83cde4078ae9cad5def28e95ae45733c476
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-07-08 11:39:40 +0530

    Time: 19 ms (7.96%) | Memory: 26 MB (5.13%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+map<int,int> mp;
+        sort(arr.begin(), arr.end());
+        for(auto it :arr){
+            mp[it[0]]++;
+            mp[it[1]]--;
+
+        }
+        int cnt=0;
+        vector<vector<int>> ans;
+        // ans.push_back({arr[0][0], arr[0][1]});
+        int st=0;
+
+        for(auto& [time, val] : mp){
+            if(cnt==0){
+                st=time;
+            }
+            cnt+=val;
+            if(cnt==0){
+                ans.push_back({st,time});
+            }
+
+        }
+return ans;
+        
+    }
+};
