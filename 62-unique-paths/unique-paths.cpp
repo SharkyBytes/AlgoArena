15871dffc1ad44b89986691046231eb1744e4783
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-16 10:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (69.71%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+int recur(int i, int j, int n, int m, vector<vector<int>> & dp){
+   
+    if(i==n-1 && j==m-1)return 1;
+    if(i>=n || j>=m)return 0;
+     if(dp[i][j]!=-1)return dp[i][j];
+
+    int x= recur(i+1, j, n, m, dp);
+    int y= recur(i, j+1, n, m, dp);
+    return dp[i][j]= x+y;
+}
+    int uniquePaths(int m, int n) {
+
+        vector<vector<int>>dp(n, vector<int> (m ,-1));
+
+        return recur(0, 0, n, m, dp);
+        
+    }
+};
