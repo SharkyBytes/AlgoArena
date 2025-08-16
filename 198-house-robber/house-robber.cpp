d42130a78e49179c8f30085e382da7fd1d4037dc
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-16 10:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.59%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int recur(int ind, int n, vector<int> & nums, vector<int> & dp){
+    if(ind==n){
+        return 0; 
+    }
+    if(dp[ind]!=-1){
+        return dp[ind];
+    }
+
+    int pick=nums[ind];
+    int notpick=0;
+    if(ind+1<n){
+        pick+=recur(ind+2, n, nums, dp);
+    }
+    notpick=recur(ind+1, n, nums, dp);
+
+    return dp[ind]=max(notpick, pick);
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<int> dp(n, -1);
+        return recur(0, n, nums, dp);
+    }
+};
