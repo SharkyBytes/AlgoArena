dbf6091dbf6ecdbfcbc6251d9f112296655d4210
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-16 16:27:01 +0530

    Time: 47 ms (37.06%) | Memory: 48.9 MB (39.33%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,14 +21,34 @@ int recur(int ind, vector<int> & arr, int sum,vector<vector<int>> & dp){
     return dp[ind][sum]=min(notake, take);
 
 }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int sum) {
 
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1 , -1));
-        int p= recur(0, coins, amount, dp);
-        if(p==INT_MAX)return -1;
-        return p;
-        
-        
+        vector<vector<int>> dp(n, vector<int>(sum+1 , INT_MAX));
+        // int p= recur(0, coins, amount, dp);
+        // if(p==INT_MAX)return -1;
+        // return p;
+
+        for(int j=1; j<=sum ; j++){
+            if(j%coins[0]==0)
+            dp[0][j]= j/coins[0];
+        }
+        for(int i=0; i< n ; i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=1 ; i< n ; i++){
+            for(int j=1; j<=sum; j++){
+                int take=INT_MAX;
+                if(coins[i]<=j){
+                    if(dp[i][j-coins[i]]!=INT_MAX)
+                    take=1+dp[i][j-coins[i]];
+                } 
+                int notake= dp[i-1][j];  
+               dp[i][j]=min(take, notake);
+            }
+        }
+
+        return dp[n-1][sum]>=INT_MAX? -1: dp[n-1][sum];
     }
 };
