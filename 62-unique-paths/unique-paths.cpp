e615fd1fd490e207fa0222d92453c9ab60aed80e
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-16 10:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.12%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -11,11 +11,22 @@ int recur(int i, int j, int n, int m, vector<vector<int>> & dp){
     int y= recur(i, j+1, n, m, dp);
     return dp[i][j]= x+y;
 }
-    int uniquePaths(int m, int n) {
+    int uniquePaths(int n, int m) {
 
         vector<vector<int>>dp(n, vector<int> (m ,-1));
 
-        return recur(0, 0, n, m, dp);
+        // return recur(0, 0, n, m, dp);
+        dp[0][0]=1;
+        for(int i=0; i<n ; i++){
+            for(int j=0; j<m ; j++){
+                if(i==0 && j==0)continue;
+               int ans=0;
+               if(i>0)ans+=dp[i-1][j];
+               if(j>0) ans+=dp[i][j-1];
+               dp[i][j]=ans;
+            }
+        }
+        return dp[n-1][m-1];
         
     }
 };
