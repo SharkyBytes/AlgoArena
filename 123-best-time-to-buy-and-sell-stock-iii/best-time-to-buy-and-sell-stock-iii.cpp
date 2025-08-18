6eec602030e271bfeeb22cf5501dd69b2e5007a1
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-18 09:14:48 +0530

    Time: 615 ms (5.01%) | Memory: 291.2 MB (5.71%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+vector<vector<vector<int>>> dp;
+
+int recur(int i , vector<int> & prices, int n , int state, int k, int curr){
+    if(i==n)return 0;
+    // if(curr==k)return 0;
+    int ans=0;
+    if(dp[i][curr][state]!=-1)return dp[i][curr][state];
+
+    if(state==0 && curr<k){
+        int buy= -prices[i]+ recur(i+1, prices, n, 1, k, curr+1);
+        int notbuy= recur(i+1, prices, n, 0, k, curr);
+
+        ans= max(buy, notbuy);
+        
+    }
+    else if(state==1){
+        int sell= prices[i]+ recur(i+1, prices, n, 0,k, curr);
+        int hold= recur(i+1, prices, n, 1, k, curr);
+
+        ans= max(sell,hold);
+    }
+
+    return dp[i][curr][state]= ans;
+
+}
+
+
+    int maxProfit(vector<int>& prices) {
+
+        int n=prices.size();
+        dp.assign(n, vector<vector<int>> (3, vector<int> (2, -1)));
+        return recur(0, prices, n, 0, 2, 0);
+        
+    }
+};
