4cda7de4582a82eeeaf3e35317827c92d03bcfe9
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-21 23:38:21 +0530

    Time: 66 ms (62.33%) | Memory: 14.2 MB (73.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -19,13 +19,28 @@ int recur(int ind, vector<int> & nums, int n , int prev){
 
     return dp[ind][prev+1]= max(take, notake);
 }
+
+
     int lengthOfLIS(vector<int>& nums) {
 
         int n=nums.size();
-        dp.assign(n, vector<int> (n+1, -1));
+        // dp.assign(n, vector<int> (n+1, -1));
 
-      return recur(0, nums, n, -1);
+    //   return recur(0, nums, n, -1);
 
-        
+    vector<int> lisDP(n, 1);
+    int maxi=INT_MIN;
+    for(int i=0; i< n ; i++){
+        for(int j=0;  j< i; j++){
+            if(nums[j]<nums[i]){
+                lisDP[i]=max(lisDP[i], 1+lisDP[j]);
+            }
+        }
+    maxi=max(lisDP[i], maxi);
+    }
+
+    return maxi;
+
+  
     }
 };
