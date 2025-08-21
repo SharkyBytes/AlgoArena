3c1134f663fb0c2b54f89c9470a8f97661a8bf84
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-21 23:29:15 +0530

    Time: 549 ms (21.64%) | Memory: 296.5 MB (13.66%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-        vector<vector<int>> dp;
-int recur(int i, vector<int> & nums, int n, int prev){
-    if(i==n)return 0;
-    if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+
+vector<vector<int>> dp;
+
+int recur(int ind, vector<int> & nums, int n , int prev){
+    if(ind==n)return 0;
+
+    if(dp[ind][prev+1]!=-1)return dp[ind][prev+1];
 
     int take=0;
-    if(prev==-1 || nums[i]>nums[prev]){
-        take=1+ recur(i+1, nums, n, i);
-    }
-    int notake= recur(i+1 , nums, n, prev);
+    int notake= recur(ind+1, nums, n, prev);
+    
+    if(prev==-1 || nums[prev]< nums[ind]){
 
-    return dp[i][prev+1]= max(take, notake);
+        take= 1+ recur(ind+1, nums, n, ind);
+
+    }
+
+    return dp[ind][prev+1]= max(take, notake);
 }
     int lengthOfLIS(vector<int>& nums) {
 
         int n=nums.size();
         dp.assign(n, vector<int> (n+1, -1));
-        return recur(0, nums, n, -1);
+
+      return recur(0, nums, n, -1);
+
         
     }
 };
