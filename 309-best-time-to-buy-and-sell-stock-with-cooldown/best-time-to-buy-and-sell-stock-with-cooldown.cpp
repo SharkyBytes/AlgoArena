95828416de96a6f86e4856c67339fa1036534131
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-23 03:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (51.65%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -25,7 +25,25 @@ int recur(int ind, vector<int> & arr, int state, int n){
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         dp.assign(n, vector<int> (2, -1));
+        dp[0][0]=-prices[0];
+        dp[0][1]=0;
+        for(int i=1; i< n ; i++){
+            // state==0
+            int buy= -prices[i];
+            if(i>=2) buy+=dp[i-2][1];
+            int notbuy= dp[i-1][0];
+            dp[i][0]=max(buy, notbuy);
 
-        return recur(0, prices, 0, n);
+            int sell= prices[i]+ dp[i-1][0];
+            int notsell= dp[i-1][1];
+
+            dp[i][1]=max(sell,  notsell);
+
+
+        }
+
+        return dp[n-1][1];
+
+        // return recur(0, prices, 0, n);
     }
 };
