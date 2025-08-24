55c3924a3b8d4e00fc570e58c34e649b1fb4bfd0
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-24 16:38:45 +0530

    Time: 1513 ms (40.67%) | Memory: 9.2 MB (85.54%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -27,8 +27,22 @@ int recur(int i , string & s, int n ){
 
         int n=s.size();
 
-        dp.assign(n, -1);
-        return recur(0, s, n);
-        
+        // dp.assign(n, -1);
+        // return recur(0, s, n);
+
+        dp.assign(n+1, -1);
+        dp[n]=0;
+        for(int i=n-1; i>=0; i--){
+            int mini=INT_MAX;
+            for(int j=i; j< n ; j++){
+                if(check(i, j , s)){
+                int cost= 1+ dp[j+1];
+                mini=min(mini, cost);
+                }
+            }
+            dp[i]=mini;
+        }
+
+        return dp[0]-1;
     }
 };
