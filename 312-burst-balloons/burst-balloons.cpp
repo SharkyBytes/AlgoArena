98ccfa7985f985157cd627688ce36b5f77518c2c
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-24 13:00:17 +0530

    Time: 105 ms (64.82%) | Memory: 13.7 MB (81.21%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -21,9 +21,26 @@ int recur(int i, int j, vector<int> & nums, int n){
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
 
-       dp.assign(n+2, vector<int> (n+2, -1));
+    //    dp.assign(n+2, vector<int> (n+2, -1));
 
-        return recur(1, n, nums, n);
+        // return recur(1, n, nums, n);
+       dp.assign(n+2, vector<int> (n+2, 0));
+       
+        for(int len=1; len<=n ; len++){
+            for(int i=1; i+len-1<=n; i++){
+
+                int j= i+len-1;
+                int maxi=INT_MIN;
+                for(int k=i;k<=j; k++){
+                    int cost = (nums[i-1]*nums[k]*nums[j+1])+ dp[i][k-1]+ dp[k+1][j];
+                    maxi=max(maxi, cost);
+                }
+                dp[i][j]=maxi;
+
+            }
+        }
+
+       return  dp[1][n];
         
     }
 };
