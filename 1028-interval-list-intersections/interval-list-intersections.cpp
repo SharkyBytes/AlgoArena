eb5392809d8ad35777d029e647e0b71f8a2326d0
Garv Agarwal <129230225+SharkyBytes@users.noreply.github.com>
2025-08-25 20:53:06 +0530

    Time: 3 ms (51.27%) | Memory: 22.9 MB (63.23%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& first, vector<vector<int>>& second) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
 
+        int n=f.size();
+        int m=s.size();
+        vector<vector<int>> ans;
         int i=0;
         int j=0;
-        vector<vector<int>> ans;
-        while(i<first.size() && j< second.size() ){
+        while(i<n && j<m){
+            int st=max(f[i][0], s[j][0]);
+            int end=min(f[i][1], s[j][1]);
 
-            int maxi=max(first[i][0], second[j][0]);
-            int mini =min(first[i][1], second[j][1]);
-            if(maxi<=mini){
-                ans.push_back({maxi,mini});
-
+             if (st <= end) {
+                ans.push_back({st, end});
             }
-            if(first[i][1]<second[j][1]){
+
+            if(f[i][1]==s[j][1]){
+                i++;
+                j++;
+            }
+
+           else if(f[i][1]<s[j][1]){
                 i++;
             }
+            else{
+                j++;
+            }
 
-            else
-            j++;
         }
-
         return ans;
         
     }
